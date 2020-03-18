74b84079f98ba88a4c50456861bef60d20a29d13
MirsonCamadro <kztempus@gmail.com>
2020-03-17 22:19:33 -0300

    fetch open weather

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -11,6 +11,12 @@ import {
     WINDY,
 } from './../../constants/weathers';
 
+const location = "Talcahuano,cl";
+const api_key  = "a2d52b8bf9125764ad1643334f9c4f66";
+const url_base_weather   = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 5,
     weatherState: RAIN,
@@ -28,6 +34,7 @@ class WeatherLocation extends Component {
         };
     };
     handdleUpdateClick = () => {
+        fetch(api_weather);
         this.setState({
             city: 'Otro Santiago'
         })
