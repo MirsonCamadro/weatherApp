80897b369007574cfabd89118c25ac7d4631aca1
MirsonCamadro <kztempus@gmail.com>
2020-03-31 11:52:33 -0300

    added react bootstrap @material-ui and api_url.js. fetching without button, componentDidMount

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
-import convert from 'convert-units';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import transformWeather from './../../services/transformWeather';
+import { api_weather } from './../../constants/api_url';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './style.css';
-import {
-    RAIN,
-} from './../../constants/weathers';
-
-const location = "Talcahuano,cl";
-const api_key  = "a2d52b8bf9125764ad1643334f9c4f66";
-const url_base_weather   = "http://api.openweathermap.org/data/2.5/weather";
-
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
-
-const data = {
-    temperature: 5,
-    weatherState: RAIN,
-    humidity: 10,
-    wind: '10 m/s',
-};
 
 class WeatherLocation extends Component {
 
@@ -26,40 +12,19 @@ class WeatherLocation extends Component {
         super();
         this.state = {
             city: 'Santiago',
-            data: data, 
+            data: null, 
         };
     };
 
-    getTemp = kelvin => {
-        return Number(
-            convert(kelvin).from("K").to("C").toFixed(2)
-            );        
-    };
-
-    getWeatherState = weather_data => {
-        return RAIN;
-    };
-
-    getData = weather_data => {
-        const { humidity, temp } = weather_data.main;
-        const { speed } = weather_data.wind;
-        const weatherState = this.getWeatherState(weather_data);
-        const temperature = this.getTemp(temp);
-
-        const data = {
-            humidity,
-            temperature,
-            weatherState,
-            wind: `${speed} m/s`,
-        }
-        return data;
+    componentDidMount() {
+        this.handdleUpdateClick();
     }
 
     handdleUpdateClick = () => {
         fetch(api_weather).then( resolve => {
             return resolve.json();
         }).then(data => {
-            const newWeather = this.getData(data);
+            const newWeather = transformWeather(data);
             console.log(newWeather);
             this.setState({
                 data: newWeather
@@ -72,8 +37,7 @@ class WeatherLocation extends Component {
         return(
             <div className="weatherLocationCont">
                 <Location city={city}/>
-                <WeatherData data = {data}/>
-                <button onClick={this.handdleUpdateClick}>Actualizar</button>
+                {data ? <WeatherData data = {data}/> : <CircularProgress/>}
             </div>
         );
     }
