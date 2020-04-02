332900aed1411d5639494619379952023b353955
MirsonCamadro <kztempus@gmail.com>
2020-04-02 11:01:04 -0300

    validate cities from LocationList as array

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities = [
+  'Talcahuano, cl',
+  'Santiago, cl',
+  'florida, us',
+  'fargo, us',
+  'arica, cl',
+  'valdivia, cl',
+]
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LocationList/>
+        <LocationList cities={cities}/>
       </header>
     </div>
   );
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import WeatherLocation from './WeatherLocation';
+import PropTypes from 'prop-types'
 
-const LocationList = () => (
-   <div>
-       <WeatherLocation city = "Santiago,cl" />
-       <WeatherLocation city = "Talcahuano,cl" />
-   </div> 
-);
+const LocationList = ({ cities }) => {
+    console.log(cities);
+    return (
+    <div>
+        <WeatherLocation city = "Santiago,cl" />
+        <WeatherLocation city = "Talcahuano,cl" />
+    </div> 
+    );
+};
+
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired,
+};
 
 export default LocationList;
